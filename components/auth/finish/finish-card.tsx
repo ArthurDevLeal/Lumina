ac3466a0f0b443eabfa8e774178bef4305dbe64a
ArthurDevLeal <arthurdevleal@gmail.com>
2025-12-12 15:09:27 -0300

    Fix auth

--- components/auth/finish/finish-card.tsx
+++ components/auth/finish/finish-card.tsx
@@ -7,19 +7,19 @@ export default function FinishCard() {
     <div className="flex flex-col gap-8">
       <div className="flex flex-col bg-card border border-border p-4 gap-4 rounded-3xl">
         <div className="flex gap-2 items-center">
-          <div className="flex items-center justify-center rounded-full size-9 bg-green-700/20">
+          <div className="flex items-center justify-center rounded-full size-9 bg-green-500/10">
             <Check className="text-green-700" />
           </div>
             <p className="font-medium text-sm">Account created</p>
         </div>
         <div className="flex gap-2 items-center">
-          <div className="flex items-center justify-center rounded-full  size-9 bg-green-700/20">
+          <div className="flex items-center justify-center rounded-full  size-9 bg-green-500/10">
             <Check className="text-green-700" />
           </div>
             <p className="font-medium text-sm">Created your wallet</p>
         </div>
         <div className="flex gap-2 items-center">
-          <div className="flex items-center justify-center rounded-full size-9 bg-green-700/20">
+          <div className="flex items-center justify-center rounded-full size-9 bg-green-500/10">
             <Check className="text-green-700" />
           </div>
             <p className="font-medium text-sm">Init your new account</p>
