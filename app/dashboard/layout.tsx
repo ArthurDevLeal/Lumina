bef701cb72c38838e9b73ec0ecfa5c7c8e8e13c6
ArthurDevLeal <arthurdevleal@gmail.com>
2026-01-02 19:45:21 -0300

    feat(dashboard): implement authentication check and redirect for dashboard access

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,10 +1,26 @@
+"use client";
 import DefaultSideBar from "@/components/sidebar/default-side-bar";
+import { useTokenStore } from "@/store/token-store";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  const { token } = useTokenStore();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!token) {
+      router.push("/auth/login");
+    }
+  }, [token, router]);
+
+  if (!token) {
+    return null;
+  }
+
   return (
     <div className="min-h-screen flex">
       <DefaultSideBar />
-
       <main className="flex-1 h-screen overflow-y-auto">{children}</main>
     </div>
   );
